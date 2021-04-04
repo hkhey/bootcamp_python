a38571b8751465e991010c2610428813291e6266
hkhey <76151779+hkhey@users.noreply.github.com>
2021-04-04 11:51:17 +0100

    Create Kata01.py

---
+++ module00/ex05/Kata01.py
@@ -0,0 +1,7 @@
+languages = {
+                "Python":"Guido van Rossum",
+                "Ruby":"Yukihiro Matsumoto",
+                "PHP":"Rasmus Lerdorf",
+            }
+for i in languages.keys():
+    print(i," was created by ",languages[i])
