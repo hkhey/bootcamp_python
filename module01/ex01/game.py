80360585e739c8b52fa49083bf9497e19795ef36
hkhey <76151779+hkhey@users.noreply.github.com>
2021-04-05 23:48:13 +0100

    Create game.py

---
+++ module01/ex01/game.py
@@ -0,0 +1,22 @@
+
+class GotCharacter:
+    def __init__(self, first_name, is_alive=True):
+        self.first_name=first_name
+        self.is_alive=is_alive
+class Stark(GotCharacter):
+    def __init__(self, first_name=None, is_alive=True):
+        super().__init__(first_name=first_name, is_alive=is_alive)
+        self.family_name="Stark"
+        self.house_words="Winter is coming"
+    def print_house_words(x):
+        print(x.house_words)
+    def die(x):
+        x.is_alive=False        
+    
+arya=Stark("Arya")
+print(arya.__dict__)
+arya.print_house_words()
+print(arya.is_alive)
+arya.die()
+print(arya.is_alive)
+print(arya.__doc__)
